42186481f34b08f0d69ff06047caf768220800a6
tpluscode <tpluscode@users.noreply.github.com>
2026-05-28 13:27:35 +0200

    refactor: eslint v9 flat config

---
+++ .changeset/eslint-9-flat-config.md
@@ -0,0 +1,5 @@
+---
+"@tpluscode/eslint-config": major
+---
+
+Updated to eslint v9 flat config
--- index.js
+++ index.js
@@ -1,61 +1,48 @@
-module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    './js.js',
-    'plugin:import/typescript'
-  ],
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
-  plugins: [
-    '@typescript-eslint/eslint-plugin'
-  ],
-  rules: {
-    indent: 'off',
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      {
-        multiline: {
-          delimiter: 'none'
-        }
-      }
-    ],
-    'no-dupe-class-members': 'off',
-    '@typescript-eslint/no-dupe-class-members': ['error'],
-    'no-redeclare': 'off',
-    '@typescript-eslint/no-redeclare': 'warn',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/consistent-type-exports': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error'
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx']
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true
-      }
-    }
-  },
-  overrides: [
-    {
-      files: [
-        '*.conf.js',
-        '*.config.js'
-      ],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off'
-      }
-    },
-    {
-      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
-      rules: {
-        'no-useless-constructor': 'off',
-        '@typescript-eslint/no-useless-constructor': 'error',
-        'no-undef': 'off'
-      }
-    }
-  ]
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import importPlugin from 'eslint-plugin-import'
+import jsConfig from './js.js'
+
+function rulesFrom(config) {
+  return config && config.rules ? config.rules : {}
 }
+
+export default [
+  ...jsConfig,
+  {
+    files: ['**/*.{ts,mts,cts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.mts', '.cts'],
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+    rules: {
+      ...rulesFrom(tsPlugin.configs.recommended),
+      ...rulesFrom(importPlugin.configs.typescript),
+
+      'no-dupe-class-members': 'off',
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'no-useless-constructor': 'off',
+      '@typescript-eslint/no-useless-constructor': 'error',
+      'no-undef': 'off',
+    },
+  },
+]
